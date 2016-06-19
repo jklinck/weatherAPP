4f863411cc443504d55c6625fac2bab0f0b5a831
jklinck <joeklinck@hotmail.com>
2016-06-19 15:11:43 -0700

    added bootstrap and got weather icon working

--- index.js
+++ index.js
@@ -2,7 +2,19 @@
 The API keys used below (openWeatherAPI and nytAPI) are located in the apiKeys.js file
 which I have added to the .gitignore file so they aren't exposed on GitHub.
 */
+
 $(function(){
+	var date = new Date();
+	var time = Number(date.getTime());
+	time /= Math.pow(10,3);
+	/* 
+	the UNIX time that date.getTime() is rendering has 3 more digits than the sunrise 
+	and sunset dates from the open weather API, the line above moves the decimal 3 
+	places to the left to account for this
+	*/
+	var sunrise;
+	var sunset;
+	var nightOrDay;
 	var latitude;
 	var longitude;
 	var request;
@@ -15,6 +27,7 @@ $(function(){
 	var GEOCODING;
 	var nytHeaderElem = $('#nytimes-header');
 	var body = $('body');
+	var icon;
 
 	navigator.geolocation.getCurrentPosition(success,error);
 
@@ -23,9 +36,7 @@ $(function(){
 		longitude=position.coords.longitude;
 		request='http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&APPID='+openWeatherAPI;
 		GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + '%2C' + longitude + '&language=en';
-
-
-
+		
 		$.getJSON(GEOCODING).done(function(location) {
             	country = location.results[0].address_components[6].short_name;
             	state = location.results[0].address_components[5].long_name;
@@ -34,29 +45,24 @@ $(function(){
 					url:request,
 					dataType:"jsonp",
 					success:function(data){
-						main=data.weather[0].main;
+						sunrise=Number(data.sys.sunrise);
+						sunset=Number(data.sys.sunset);
+						//main=data.weather[0].main;
+						icon=data.weather[0].icon;
+						picture="url(http://openweathermap.org/img/w/"+icon+".png)";
 						description=data.weather[0].description;
-						console.log("description: ",description)
 						kelvin=data.main.temp;
 						celsius=(kelvin-273.15).toFixed(1);
 						farenheit=(1.8*(kelvin-273)+32).toFixed(1);
-						$("#city").html(city);
-						$("#state").html(state);
+						$("#cityState").html(city+", "+state);
 						$("#country").html(country);
 						$("#weather").html(farenheit+"&#176 F");
 						$("#description").html(description);
-						if(main=="Clouds"){
-							$("#main").addClass("wi wi-night-cloudy");
-						}
-						if(main=="Rain"){
-							$("#main").addClass("wi wi-night-rain");
-						}
-					}
+						$(".icon").css({"background-image":picture});
+					} // closes success callback from $.ajax
 				}) // closing $.ajax
-
 			// NYT API
 		    var nytUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?&q='+city+','+state+'&page=0&api-key='+nytAPI;
-
 		    $.getJSON(nytUrl).done(function(data){
 		        var articles=[];
 		        $.each(data.response.docs,function(key){
@@ -69,7 +75,6 @@ $(function(){
 			    }).error(function(){
 			        $(nytHeaderElem).text("New York Times articles could not be loaded.");
 			    });
-                
             })  // closing $.getJSON(GEOCODING)
 	} // end of success function
 
