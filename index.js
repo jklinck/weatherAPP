0c3e6df2c5a4011d5a659a88515141fed1c96fef
jklinck <joeklinck@hotmail.com>
2016-06-21 02:42:15 -0700

    got rid of unused wether icon link and fixed up some syntax in index.js

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ which I have added to the .gitignore file so they aren't exposed on GitHub.
 $(function(){
 	var latitude;
 	var longitude;
-	var request;
+	var weatherURL;
+	var mapsURL;
+	var nytUrl;
 	var city;
 	var farenheit;
 	var celsius;
 	var kelvin;
 	var main;
 	var description;
-	var GEOCODING;
 	var nytHeaderElem = $('#nytimes-header');
 	var body = $('body');
 	var icon;
@@ -23,8 +24,8 @@ $(function(){
 	function success(position){
 		latitude=position.coords.latitude;
 		longitude=position.coords.longitude;
-		request='http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&APPID='+openWeatherAPI;
-		geoCoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + '%2C' + longitude + '&language=en';
+		weatherURL='http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&APPID='+openWeatherAPI;
+		mapsURL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + '%2C' + longitude + '&language=en';
 		gMaps();
 	} 
 
@@ -34,27 +35,23 @@ $(function(){
 	
 	// Open Weather Map API
 	function openWeather(){
-			$.ajax({
-					url:request,
-					dataType:"jsonp",
-					success:function(data){
-						icon=data.weather[0].icon;
-						picture="url(http://openweathermap.org/img/w/"+icon+".png)";
-						description=data.weather[0].description;
-						kelvin=data.main.temp;
-						celsius=(kelvin-273.15).toFixed(1);
-						farenheit=(1.8*(kelvin-273)+32).toFixed(1);
-						$("#cityState").html(city+", "+state);
-						$("#country").html(country);
-						$("#weather").html(farenheit+"&#176 F");
-						$("#description").html(description);
-						$(".icon").css({"background-image":picture});
-					} 
-				}) 
+			$.getJSON(weatherURL).done(function(data){
+					icon=data.weather[0].icon;
+					picture="url(http://openweathermap.org/img/w/"+icon+".png)";
+					description=data.weather[0].description;
+					kelvin=data.main.temp;
+					celsius=(kelvin-273.15).toFixed(1);
+					farenheit=(1.8*(kelvin-273)+32).toFixed(1);
+					$("#cityState").html(city+", "+state);
+					$("#country").html(country);
+					$("#weather").html(farenheit+"&#176 F");
+					$("#description").html(description);
+					$(".icon").css({"background-image":picture});
+			})
 	}
 	// New York Times API
 	function nyt(){
-		 var nytUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?&q='+city+','+state+'&page=0&api-key='+nytAPI;
+		    nytUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?&q='+city+','+state+'&page=0&api-key='+nytAPI;
 		    $.getJSON(nytUrl).done(function(data){
 		        var articles=[];
 		        $.each(data.response.docs,function(key){
@@ -70,7 +67,7 @@ $(function(){
 	}
 	// Google Maps API
 	function gMaps(){
-		$.getJSON(geoCoding).done(function(location) {
+		$.getJSON(mapsURL).done(function(location) {
             	country = location.results[0].address_components[6].short_name;
             	state = location.results[0].address_components[5].long_name;
             	city = location.results[0].address_components[3].long_name;
@@ -79,15 +76,14 @@ $(function(){
             })  
 	}
 
-
 	// toggle between Farenheit and Celsius
 	var isClicked = false;
-	$('#weather').click(function() {
-		if (isClicked == false) {
+	$('#weather').on("click",function(){
+		if (isClicked == false){
 			$(this).html(celsius+"&#176 C");
 			isClicked = true;
 		}
-		else {
+		else{
 			$(this).html(farenheit+"&#176 F");
 			isClicked = false;
 		}
